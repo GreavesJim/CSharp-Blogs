c1fe55f61f63d7f5ed8f92ef6fc810e9d9dc8cc9
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2020-02-05 23:11:02 -0700

    init

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace csharp_blogs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogsController : ControllerBase
+    {
+        private readonly BlogsService _ks;
+        public BlogsController(BlogsService ks)
+        {
+            _ks = ks;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Blog>> Get()
+        {
+            try
+            {
+                return Ok(_ks.Get());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            };
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult<Blog> Post([FromBody] Blog newBlog)
+        {
+            try
+            {
+                var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                newBlog.UserId = userId;
+                return Ok(_ks.Create(newBlog));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
